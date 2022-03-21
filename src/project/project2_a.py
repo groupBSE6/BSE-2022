8ccfb1b1bec69cd8cb995a4c4afeff81cb072966
edgar040 <bwambaleedgar28@gmail.com>
2022-03-21 15:18:41 +0300

    project

--- src/project/project2_a.py
+++ src/project/project2_a.py
@@ -7,16 +7,19 @@ except:
 file_name = open("measles.txt")
 output_file = input("Enter the output file : ")
 year = input("Enter the year : ")
+
+
 with open(output_file, "w") as file:
-    for line in file_name:
+    for lines in file_name:
+
         if year.lower() == "all" or year == "": #All, all, "Empty Input"
-            file.write(line)
-        elif int(year) == int(line[88:]):# year
-            file.write(line)
-        elif int(year) == int(line[88]):
-            file.write(line)
-        elif int(year) == int(line[88:90]):
-            file.write(line)
-        elif int(year) == int(line[88:91]):
-            file.write(line)
-        
+            file.writelines(lines)
+        elif int(year) == int(lines[88:]):# year
+            file.writelines(lines)
+        elif int(year) == int(lines[88]):
+            file.writelines(lines)
+        elif int(year) == int(lines[88:90]):
+            file.writelines(lines)
+        elif int(year) == int(lines[88:91]):
+            file.writelines(lines)
+#
